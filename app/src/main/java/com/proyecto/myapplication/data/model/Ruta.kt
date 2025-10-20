610c455a529a0c32daea7a26a54a1ac4d2ae9610
Leonardo902-alan <leonardo.alanya@tecsup.edu.pe>
2025-10-19 22:56:36 -0500

    3er Commit

--- app/src/main/java/com/proyecto/myapplication/data/model/Ruta.kt
+++ app/src/main/java/com/proyecto/myapplication/data/model/Ruta.kt
@@ -1,2 +1,23 @@
 package com.proyecto.myapplication.data.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import com.proyecto.myapplication.data.database.Converters
+
+@Entity(tableName = "rutas")
+@TypeConverters(Converters::class)
+data class Ruta(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+
+    val origen: String,
+
+    val destino: String,
+
+    val tiempoEstimado: Int, // en minutos
+
+    val estacionesIntermedias: List<String>,
+
+    val favorito: Boolean = false
+)
