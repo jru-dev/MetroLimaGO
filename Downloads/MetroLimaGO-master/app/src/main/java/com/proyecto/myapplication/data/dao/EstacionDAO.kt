42327104b1940a8b1e5ac8a987397f947a477f33
gmarcoDev <gian.pasco@tecsup.edu.pe>
2025-10-20 21:27:01 -0500

    Estacion.kt

--- Downloads/MetroLimaGO-master/app/src/main/java/com/proyecto/myapplication/data/dao/EstacionDAO.kt
+++ Downloads/MetroLimaGO-master/app/src/main/java/com/proyecto/myapplication/data/dao/EstacionDAO.kt
@@ -1,2 +1,32 @@
 package com.proyecto.myapplication.data.dao
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import androidx.room.Delete
+import com.proyecto.myapplication.data.entity.Estacion
+
+@Dao
+interface EstacionDao {
+
+    // Insertar una nueva estación
+    @Insert
+    suspend fun insertarEstacion(estacion: Estacion)
+
+    // Obtener todas las estaciones
+    @Query("SELECT * FROM estaciones")
+    suspend fun obtenerTodasLasEstaciones(): List<Estacion>
+
+    // Obtener una estación por su ID
+    @Query("SELECT * FROM estaciones WHERE id = :id")
+    suspend fun obtenerEstacionPorId(id: Long): Estacion?
+
+    // Actualizar una estación
+    @Update
+    suspend fun actualizarEstacion(estacion: Estacion)
+
+    // Eliminar una estación
+    @Delete
+    suspend fun eliminarEstacion(estacion: Estacion)
+}
